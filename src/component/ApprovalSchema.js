7ebb0ddd5f354eae28ac3f47f2700bf79507221c
Sophie Kolbe <soko@Sophies-Air.fritz.box>
2020-08-02 10:56:55 +0200

    refactor data fetching and add edit mode for approval schema

--- src/component/ApprovalSchema.js
+++ src/component/ApprovalSchema.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from "axios";
 import './ApprovalSchema.css';
 
 class ApprovalSchema extends React.Component {
@@ -13,64 +12,75 @@ class ApprovalSchema extends React.Component {
       this.handleTeamChange = this.handleTeamChange.bind(this);
   }
 
-  componentDidMount() {
-      this.getData('teams') 
-      this.getData('users') 
+  async componentDidMount() {
+    //TODO: put this into main app component
+    //enable cors via heroku
+    const source = 'https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/spx-development/contents'
+    const users = await fetch(`${source}/users`)
+    const teams = await fetch(`${source}/teams`)
+    const jsonUsers = await users.json();
+    const jsonTeams = await teams.json();
+    this.setState({ users: jsonUsers})
+    this.setState({ teams: jsonTeams})
   };
 
-  getData = (attribute) => {
-    axios
-      .get(
-          //cors enabled: https://cors-anywhere.herokuapp.com/
-          `https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/spx-development/contents/${attribute}`
-      )
-      .then(response => {
-          this.setState({ [attribute]: response.data })
-      })
-      .catch(error => {
-          console.log(`Error Occured when Fetching ${attribute}`, error.message)
-      });
-  }
-
-  handleTeamChange (event) {
-    const value = event.target.value
-    this.setState({ selectedTeam: this.state.teams.find( _ => _.name === value)})
+  handleTeamChange (team) {
+    this.setState({ selectedTeam: team})
   }
 
   findUserbyId (id) {
-    return this.state.users.find( _ => _.id === id)
+    return this.state.users.find( _ => _.id === id) 
   }
-  renderTeamSelector () {
+
+  renderApprovalSchema (team) {
     return (
-      <React.Fragment>
-        <label>Select a team:</label>
-        <select 
-          className="select-team" 
-          value={this.state.selectedTeam} 
-          onChange={this.handleTeamChange}>
-          { this.state.teams.map( _ => <option key={_.id}>{_.name}</option>) }
-      </select>
-     </React.Fragment>
+        <div className="approval-schema">
+          <h3>Set up approvers</h3>
+          <div>Who can approve for team {team.name}?</div>
+          <div className="controls">
+              <button className="secondary-action" onClick={() => this.handleTeamChange({})}>Cancel</button>  
+              <button className="primary-action" onClick={() => this.handleTeamChange({})}>Save Approval Flow</button>
+          </div>
+        </div>
     )
   }
 
-  renderTeam (team) {
+  renderUserAndApprovers (team) {
+    const approversOfTeam = []
     const usersOfTeam = team.users.map( _ => this.findUserbyId(_)).splice(0,3)
     return (
       <React.Fragment>
-          <h1> Team {team.name}</h1>
-          { usersOfTeam.map( _ => <div key={_.id}>{ `${_.first_name} ${_.last_name}`}</div>) }
+         <div className="team-title"> 
+            Team {team.name} 
+             <button className="primary-action" onClick={() => this.handleTeamChange(team)}>Edit Approvals</button>
+          </div>
+        <div>
+            Users: { team.users.length > 3 ? <span className="total-users">(3 out of {team.users.length})</span> : null} 
+          { usersOfTeam.map( _ => <ul key={_.id}>{`${_.first_name} ${_.last_name}`}</ul>) }
+          </div>
+          <div>
+          Approvers:
+          { approversOfTeam.length > 0 
+            ? approversOfTeam.map( _ => <ul key={_.id}>{`${_.first_name} ${_.last_name} `}</ul>) 
+            : <ul>No approver yet</ul> }  
+        </div>  
       </React.Fragment>
     )
   }
 
-  render (){
-    const team = this.state.selectedTeam
-
+  renderTeam (team) {
+    const isSelected = this.state.selectedTeam.name === team.name
     return (
-      <div className="approval-schema">
-        { this.renderTeamSelector() }
-        { team.name ? this.renderTeam(team) : null }
+      <div className="team" key={team.name}>
+          { isSelected ? this.renderApprovalSchema(team) : this.renderUserAndApprovers(team)}    
+      </div>
+    )
+  }
+
+  render (){
+    return (
+      <div className="approval-manager">
+        { this.state.teams.length ? this.state.teams.map( team => this.renderTeam(team)) : <span> No data loaded.</span> }
       </div>
     );
   }
